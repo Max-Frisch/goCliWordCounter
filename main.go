8af8e11acc94b6233e328b00722734bd42760c79
Max <max-frisch@gmx.net>
2022-12-08 13:57:27 +0700

    Main program plus test
    Written the main application plus a test

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
-	fmt.Println("testing..")
+	// Calling the count function to count the number of words
+	// received from the Standard Input and printing it out
+
+	fmt.Println(count(os.Stdin))
+}
+
+func count(r io.Reader) int {
+	// A scanner is used to read text from a Reader (such as files)
+	scanner := bufio.NewScanner(r)
+
+	// Define the scanner split type to words (default is split by lines)
+	scanner.Split(bufio.ScanWords)
+
+	// Defining a Counter
+	wc := 0
+
+	// For every word scanned, increment the counter
+	for scanner.Scan() {
+		wc++
+	}
+
+	// Return the total
+	return wc
+
 }
